72445bfe4e5ae955a0a663d601c3975cbefb00f0
Masami <msm-k0304@hotmail.com>
2023-07-08 14:58:54 +1200

    first commit

---
+++ SchoolFormsApp/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace SchoolFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textSchoolName = new System.Windows.Forms.TextBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textCity = new System.Windows.Forms.TextBox();
+            this.textState = new System.Windows.Forms.TextBox();
+            this.textZip = new System.Windows.Forms.TextBox();
+            this.textPhoneNumber = new System.Windows.Forms.TextBox();
+            this.textTwitter = new System.Windows.Forms.TextBox();
+            this.pressToTest = new System.Windows.Forms.Button();
+            this.btnTestTeacher = new System.Windows.Forms.Button();
+            this.btnTestStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(64, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 16);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "School Name";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(67, 90);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 16);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Address";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(67, 144);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 16);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "City";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(67, 197);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(38, 16);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "State";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(70, 256);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(26, 16);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Zip";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(70, 307);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(97, 16);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "Phone Number";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(70, 356);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(46, 16);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "Twitter";
+            // 
+            // textSchoolName
+            // 
+            this.textSchoolName.Location = new System.Drawing.Point(175, 43);
+            this.textSchoolName.Name = "textSchoolName";
+            this.textSchoolName.Size = new System.Drawing.Size(100, 22);
+            this.textSchoolName.TabIndex = 7;
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(175, 87);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(100, 22);
+            this.textAddress.TabIndex = 8;
+            // 
+            // textCity
+            // 
+            this.textCity.Location = new System.Drawing.Point(175, 144);
+            this.textCity.Name = "textCity";
+            this.textCity.Size = new System.Drawing.Size(100, 22);
+            this.textCity.TabIndex = 9;
+            // 
+            // textState
+            // 
+            this.textState.Location = new System.Drawing.Point(175, 197);
+            this.textState.Name = "textState";
+            this.textState.Size = new System.Drawing.Size(100, 22);
+            this.textState.TabIndex = 10;
+            // 
+            // textZip
+            // 
+            this.textZip.Location = new System.Drawing.Point(175, 250);
+            this.textZip.Name = "textZip";
+            this.textZip.Size = new System.Drawing.Size(100, 22);
+            this.textZip.TabIndex = 11;
+            // 
+            // textPhoneNumber
+            // 
+            this.textPhoneNumber.Location = new System.Drawing.Point(175, 301);
+            this.textPhoneNumber.Name = "textPhoneNumber";
+            this.textPhoneNumber.Size = new System.Drawing.Size(100, 22);
+            this.textPhoneNumber.TabIndex = 12;
+            // 
+            // textTwitter
+            // 
+            this.textTwitter.Location = new System.Drawing.Point(175, 356);
+            this.textTwitter.Name = "textTwitter";
+            this.textTwitter.Size = new System.Drawing.Size(100, 22);
+            this.textTwitter.TabIndex = 13;
+            // 
+            // pressToTest
+            // 
+            this.pressToTest.Location = new System.Drawing.Point(99, 418);
+            this.pressToTest.Name = "pressToTest";
+            this.pressToTest.Size = new System.Drawing.Size(160, 29);
+            this.pressToTest.TabIndex = 14;
+            this.pressToTest.Text = "Press to Test";
+            this.pressToTest.UseVisualStyleBackColor = true;
+            this.pressToTest.Click += new System.EventHandler(this.pressToTest_Click);
+            // 
+            // btnTestTeacher
+            // 
+            this.btnTestTeacher.Location = new System.Drawing.Point(99, 483);
+            this.btnTestTeacher.Name = "btnTestTeacher";
+            this.btnTestTeacher.Size = new System.Drawing.Size(160, 23);
+            this.btnTestTeacher.TabIndex = 15;
+            this.btnTestTeacher.Text = "Test Teacher";
+            this.btnTestTeacher.UseVisualStyleBackColor = true;
+            this.btnTestTeacher.Click += new System.EventHandler(this.btnTestTeacher_Click);
+            // 
+            // btnTestStudent
+            // 
+            this.btnTestStudent.Location = new System.Drawing.Point(99, 525);
+            this.btnTestStudent.Name = "btnTestStudent";
+            this.btnTestStudent.Size = new System.Drawing.Size(160, 23);
+            this.btnTestStudent.TabIndex = 16;
+            this.btnTestStudent.Text = "Test Student";
+            this.btnTestStudent.UseVisualStyleBackColor = true;
+            this.btnTestStudent.Click += new System.EventHandler(this.btnTestStudent_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(402, 573);
+            this.Controls.Add(this.btnTestStudent);
+            this.Controls.Add(this.btnTestTeacher);
+            this.Controls.Add(this.pressToTest);
+            this.Controls.Add(this.textTwitter);
+            this.Controls.Add(this.textPhoneNumber);
+            this.Controls.Add(this.textZip);
+            this.Controls.Add(this.textState);
+            this.Controls.Add(this.textCity);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.textSchoolName);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtTwitter;
+        private System.Windows.Forms.Button btnPushToTest;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textSchoolName;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textCity;
+        private System.Windows.Forms.TextBox textState;
+        private System.Windows.Forms.TextBox textZip;
+        private System.Windows.Forms.TextBox textPhoneNumber;
+        private System.Windows.Forms.TextBox textTwitter;
+        private System.Windows.Forms.Button pressToTest;
+        private System.Windows.Forms.Button btnTestTeacher;
+        private System.Windows.Forms.Button btnTestStudent;
+    }
+}
+
---
+++ SchoolLibrary/EnglishPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    internal class EnglishPaper
+    {
+        public string Title { get; set; }
+        public string MinimumWordCount { get; set; }
+        public string PapetTest { get; set; }
+    }
+}
